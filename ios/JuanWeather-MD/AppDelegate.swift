8e74ccd6799c07fce2538f5b380aec0349f143cd
Juan Carlos Miguel T. Basa <143696331+jayceebasa@users.noreply.github.com>
2025-09-29 16:09:57 +0800

    Rename app to JuanWeather-MD across platforms
    Updated app name, display name, and related identifiers from 'MyApp' to 'JuanWeather-MD' in Android, iOS, and configuration files. Added new iOS target files for JuanWeather-MD and updated project structure and Podfile accordingly. Also modified the homepage settings icon SVG for improved clarity.

--- ios/JuanWeather-MD/AppDelegate.swift
+++ ios/JuanWeather-MD/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "JuanWeather",
+      withModuleName: "JuanWeather-MD",
       in: window,
       launchOptions: launchOptions
     )
